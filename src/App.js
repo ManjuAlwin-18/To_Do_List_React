f641fbb79aad0b442d33e86a63d8cb8dbf15dd03
Manju Davis <manjudavis18@gmail.com>
2023-02-09 14:33:10 +1300

    Todolist almost finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  
+  const [toDos,setTodos] = useState([])
+  const [toDo,setTodo] =useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +14,41 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(), text:toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Car Wash</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {
+          toDos.map((obj)=>{
+
+         return(
+          <div className="todo">
+            <div className="left">
+              <input onChange={(e)=>{
+                  console.log(e.target.checked)
+                  console.log(obj)
+                  setTodos(toDos.filter(obj2=>{
+                    if(obj2.id===obj.id){
+                      obj2.status=e.target.checked
+                    }
+                    return obj2
+                  }))
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+            <i id={obj.id} className="fas fa-times" onClick={(e)=>{
+              let index= toDos.findIndex(obj=>{return obj.id===e.target.id})
+              if (index !== -1) {
+                toDos.splice(index, 1);
+                setTodos([...toDos]);
+              }
+              
+            }}>
+            </i>
+            </div>
+          </div> )
+         }) }
       </div>
     </div>
   );
